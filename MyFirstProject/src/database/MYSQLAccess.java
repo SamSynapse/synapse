d3a33ef7b1c4cb0444df7498e4f5b92615bad820
SamSynapse <synapse.sam@gmail.com>
2017-05-20 17:05:03 +0200

    test

--- MyFirstProject/src/database/MYSQLAccess.java
+++ MyFirstProject/src/database/MYSQLAccess.java
@@ -1,51 +1,98 @@
 package database;
 
-public class db_connect {
+import java.sql.*;
 
+public class MYSQLAccess {
 	
-	
-	
-	public void db_connection() {
-	
-		String db_url = "jdbc:oracle:thin:@sgremedydb.scigames.at:7262:remdydb";
-		String db_usr = "usr";
-		String db_pw = "pwd";
-	
-		Connection conn;
-	
-		try {
-		DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-	
-		conn = DriverManager.getConnection(db_url, db_usr, db_pw);
-		String call = "{ ? = call split_rc_worklog_to_ref_all (P_REMEDY_NR => ? " +
-		" , P_ISSUE_TYP => ?) } ";
-		CallableStatement cstmt = conn.prepareCall(call);
-		cstmt.setQueryTimeout(1800);
-		cstmt.registerOutParameter(1, OracleTypes.CURSOR);
-		cstmt.setString(2, remedy_nr);
-		cstmt.setString(3, issuetyp);
-		cstmt.executeUpdate();
-		ResultSet rset = (ResultSet)cstmt.getObject (1);
-		//System.out.println("From RD" + Remedy_id + " to " + jira_id);
-		// Dump the cursor
-	
-		while (rset.next ())
-		{
-		String Worklog = rset.getString("WORKLOG");
-		comment_for_admins = false;
-		String newdate = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss").format(rset.getTimestamp("DATUM"));
-		}
-	
-		// Close all the resources
-		cstmt.close();
-		rset.close();
-		conn.close();
-		} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		}
-	
-	}
+    public static void main(String[] args)
+    {
+    	/*
+    	 * Serverdaten
+    	 */
+    	String port 	= "80";
+    	String website 	= "localhost";
+    	
+        String url = "jdbc:mysql://"+website+":"+port+"/";
+
+        /*
+         *	Logindaten
+         */
+        String user = "root";
+        String password = "";
+
+        
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection con = DriverManager.getConnection(url, user, password);
+
+            Statement stt = con.createStatement();
+
+            /**
+             * Create and select a database for use. 
+             */
+            stt.execute("CREATE DATABASE IF NOT EXISTS test");
+            stt.execute("USE test");
+
+            /**
+             * Create an example table
+             */
+            stt.execute("DROP TABLE IF EXISTS people");
+            stt.execute("CREATE TABLE people (" +
+                    "id BIGINT NOT NULL AUTO_INCREMENT,"
+                    + "fname VARCHAR(25),"
+                    + "lname VARCHAR(25),"
+                    + "PRIMARY KEY(id)"
+                    + ")");
+
+            /**
+             * Add entries to the example table
+             */
+            stt.execute("INSERT INTO people (fname, lname) VALUES" + 
+                    "('Joe', 'Bloggs'), ('Mary', 'Bloggs'), ('Jill', 'Hill')");
+
+            /**
+             * Query people entries with the lname 'Bloggs'
+             */
+            ResultSet res = stt.executeQuery("SELECT * FROM people WHERE lname = 'Bloggs'");
+
+            /**
+             * Iterate over the result set from the above query
+             */
+            while (res.next())
+            {
+                System.out.println(res.getString("fname") + " " + res.getString("lname"));
+            }
+            System.out.println("");
+
+            /**
+             * Same as the last query, but using a prepared statement. 
+             * Prepared statements should be used when building query strings
+             * from user input as they protect against SQL injections
+             */
+            PreparedStatement prep = con.prepareStatement("SELECT * FROM people WHERE lname = ?");
+            prep.setString(1, "Bloggs");
+
+            res = prep.executeQuery();
+            while (res.next())
+            {
+                System.out.println(res.getString("fname") + " " + res.getString("lname"));
+            }
+
+            /**
+             * Free all opened resources
+             */
+            res.close();
+            stt.close();
+            prep.close();
+            con.close();
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 	
 	
 	
